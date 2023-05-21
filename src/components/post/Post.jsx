84fec2fc22ed6b8b91d57912bbc98491aa16fc85
amatiroo <rooamati@gmail.com>
2023-05-21 18:39:56 -0500

    all segments filled

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,32 +1,34 @@
 import React from './post.css'
 import { MoreVert } from '@mui/icons-material'
+import { Users } from '../../dummyData'
 
-export default function Post() {
+export default function Post({post}) {
+    
   return (
     <div className='post'>
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
-                    <span className="postUserName">Safak Ria</span>
-                    <span className="postDate">5 min ago</span>
+                    <img src={Users.filter(u=>u.id===post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                    <span className="postUserName">{Users.filter(u=>u.id===post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert/>
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! It's my first post :)</span>
-                <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+                <span className="postText">{post?.desc}</span>
+                <img src={post.photo} alt="" className="postImg" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img  className='likeIcon' src="/assets/like.png" alt="" />
                     <img className='likeIcon' src="/assets/heart.png" alt="" />
-                    <span className="postLikeCounter">32 people liked it</span>
+                    <span className="postLikeCounter">{post.like} people liked it</span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">9 comments</span>
+                    <span className="postCommentText">{post.comment} comments</span>
                 </div>
             </div>
         </div>
