df9c472905e5cc70d69634356d1b61b2e48e64f6
amatiroo <rooamati@gmail.com>
2023-06-13 13:05:55 -0700

    Context API and Hooks

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,8 +3,26 @@ import Rightbar from '../../components/rightbar/Rightbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Topbar from '../../components/topbar/Topbar'
 import React from './profile.css'
+import { useState , useEffect } from 'react'
+import axios from "axios"
 
 export default function Profile() {
+  const PF =  process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user,setUser] = useState({})
+
+  useEffect(()=>{
+    const fetchUser = async () =>{
+      try{
+        const res = await axios.get(`/users?username=Oswald`);
+        setUser(res.data)
+      } catch (error) {
+        console.error(error);
+      }
+      
+    };
+    fetchUser();
+},[]) 
+
   return (
     <>
         <Topbar/>
@@ -15,18 +33,18 @@ export default function Profile() {
             <div className="profileCover">
 
                 
-            <img src="/assets/post/3.jpeg" alt="" className="coverPicture" />   
-            <img src="/assets/person/1.jpeg" alt="" className="profileUserImg" /> 
+            <img src={user.coverPicture || PF+"post/1.jpeg"} alt="" className="coverPicture" />   
+            <img src={user.profilePicture || PF+"person/oswald.jpg"} alt="" className="profileUserImg" /> 
 
             </div>
             <div className="profileInfo">
-                <h4 className='profileInfoName'>Safak Kocaoglu</h4>
-                <span className='profileInfoDesc'>Hello my Friends!!</span>
+                <h4 className='profileInfoName'>{user.username}</h4>
+                <span className='profileInfoDesc'>{user.desc}</span>
             </div>
             </div>
             <div className="profileRightBottom">
-            <Feed/>
-        <Rightbar profile/>
+            <Feed username="Oswald"/>
+        <Rightbar user={user}/>
             </div>
         
         </div>
